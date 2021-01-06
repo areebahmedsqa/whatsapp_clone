8eccf7a633a487714dbc7732490a27429ab28c5c
Areeb Ahmed <70444367+areebahmedsqa@users.noreply.github.com>
2021-01-07 02:36:45 +0500

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from "react";
 import './App.css';
+import Sidebar from "./Sidebar";
 
 
 function App() {
   return (
     //BEM naming convention 
     <div className="app">
-      <h1>Let's build a WhatsApp clone!</h1>
-
       <div className="app__body">
-      {/* Sidebar */}
+      <Sidebar></Sidebar>
       {/* Chat */}
       </div>
     </div>
